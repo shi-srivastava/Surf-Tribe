fd5fea790c42f51aa3bd019ea9dc9b554328e3ab
nishisharma311 <nishisharmaanil@gmail.com>
2021-06-13 13:05:18 +0530

    database

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
 const app = express();
+
+
+const connectDB = require('./db/conn');
+
+
+//connect database
+connectDB();
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
@@ -11,7 +20,7 @@ app.set("view engine", "hbs");
 app.set("views", template_path);
 hbs.registerPartials(partials_path);
 app.get("/",(req,res) => {
-    res.render("index");
+    res.render("register");
 });
 app.get("/register",(req,res) => {
     res.render("register");
