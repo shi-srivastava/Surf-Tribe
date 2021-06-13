ae568c85a99daf3c69468079af23100db1cd3af5
PriyanshiChandra <chandrapriyanshi@gmail.com>
2021-06-13 13:24:27 +0530

    VolunteerSchema

---
+++ backend/src/models/Register.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const volunteerSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    password2:{
+        type:String,
+        required:true
+    }
+
+})
+
+const Register = new mongoose.model("Register",volunteerSchema);
+
+module.exports = Register;
