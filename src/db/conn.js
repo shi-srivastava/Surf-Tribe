d693ba5081fe27c8227b417b95e362fe39e8f22e
Shivakshi <shivakshisheel.srivastava@gmail.com>
2021-07-07 11:51:41 +0530

    aaaaaa

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://nishi_311:Nishi311@devconnector.brt9g.mongodb.net/Registers?retryWrites=true&w=majority";
+const uri = process.env.URI;
 const connectDB = async () => {
     try {
         await mongoose.connect( uri, {
+            useNewUrlParser :true,
             useUnifiedTopology : true,
-            useCreateIndex :true,
-            useNewUrlParser :true
+            useCreateIndex :true
+            
         });
         console.log("MongoDB connected ...");
     } catch(err) {
